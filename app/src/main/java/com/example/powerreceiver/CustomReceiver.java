d58c7180a94718f19d866c3b545c3ac7d8669ce8
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2021-10-09 11:27:20 +0100

    Initial commit.

---
+++ app/src/main/java/com/example/powerreceiver/CustomReceiver.java
@@ -0,0 +1,39 @@
+package com.example.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    String toastMessage;
+    // String constant that defines the custom broadcast Action.
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+
+        if (intentAction != null) {
+            toastMessage = "unknown intent action";
+            switch (intentAction){
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = "Power connected!";
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = "Power disconnected!";
+                    break;
+                case ACTION_CUSTOM_BROADCAST:
+                    toastMessage = "Custom Broadcast Received";
+                    break;
+            }
+
+            //Display the toast.
+        }
+
+        Toast.makeText(context, toastMessage, Toast.LENGTH_LONG).show();
+
+    }
+}
